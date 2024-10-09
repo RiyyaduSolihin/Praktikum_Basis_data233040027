aaa982f3779ffb1b5985b4711ffec6a01e4acb25
RiyyaduSolihin <riyyadu1234@gmail.com>
2024-10-09 12:00:13 +0700

    latihan 1 insert

---
+++ latihan1.sql
@@ -0,0 +1,8 @@
+
+
+INSERT INTO Mahasiswa (ID, NIM, Nama, Jurusan, Tanggallahir)
+VAlUES (2, 233040027, 'Riyyadu Solihin' , 'Teknik Informatika', '2004-11-11'),
+(3, 233040025, 'Riki RIKANDI' , 'Teknik Informatika', '2002-02-15'),
+(4, 203040190, 'Budi Sadui' , 'Teknik Informatika', '2005-02-15'),
+(5, 203040180, 'Budi Sutosa' , 'Teknik Informatika', '2001-02-15'),
+(6, 203040160, 'jaya santosa' , 'Teknik Informatika', '2007-02-15');
